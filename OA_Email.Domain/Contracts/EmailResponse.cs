4a90dd416c135530af094603756476270a8bad80
Edivandro <edileal1@gmail.com>
2023-08-23 10:59:50 -0300

    Contracts e profile

---
+++ OA_Email.Domain/Contracts/EmailResponse.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace OA_Email.Domain.Contracts
+{
+    public class EmailResponse
+    {
+        [Required]
+        public string Codigo { get; set; }
+    }
+}
---
+++ OA_Email.Domain/Profiles/CodigoEmailProfile.cs
@@ -0,0 +1,15 @@
+﻿using AutoMapper;
+using OA_Email.Domain.Contracts;
+using OA_Email.Domain.Entities;
+
+namespace OA_Email.Api.Profiles
+{
+    public class CodigoEmailProfile : Profile
+    {
+        public CodigoEmailProfile()
+        {
+            CreateMap<EmailRequest, CodigoEmail>().ReverseMap();
+            CreateMap<EmailResponse, CodigoEmail>().ReverseMap();
+        }
+    }
+}
