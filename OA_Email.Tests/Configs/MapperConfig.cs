d66f78a1a9019789abf03a343a1de197fc216ade
Edivandro <edileal1@gmail.com>
2023-08-29 13:48:23 -0300

    CodigoServiceTest

---
+++ OA_Email.Tests/Configs/MapperConfig.cs
@@ -0,0 +1,22 @@
+﻿using AutoMapper;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace OA_Email.Tests.Configs
+{
+    public static class MapperConfig
+    {
+        public static IMapper Get()
+        {
+            var mockMapper = new MapperConfiguration(cfg =>
+            {
+                cfg.AddProfile(new Api.Profiles.CodigoEmailProfile());
+            });
+
+            return mockMapper.CreateMapper();
+        }
+    }
+}
